6357201776de7c3e21a05f429a304c645c94ba72
GuiFranBento <142411766+GuiFranBento@users.noreply.github.com>
2023-10-20 18:26:37 -0300

    site com os projetos

--- dark/script.js
+++ dark/script.js
@@ -2,3 +2,23 @@
     chk.addEventListener('change', () => {
         document.body.classList.toogle('dark')
     })
+
+
+
+let slides = document.querySelectorAll('.pdiv');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active')
+    index = (index + 1 ) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active')
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
+setInterval(next, 7000)
