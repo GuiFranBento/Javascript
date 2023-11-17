e14d50ad318f2dc92a97ca145f30cc79ad3661bf
GuiFranBento <142411766+GuiFranBento@users.noreply.github.com>
2023-11-17 19:37:10 -0300

    meu site

--- dark/script.js
+++ dark/script.js
@@ -1,6 +1,6 @@
 const switchToogle = document.querySelector('.switch-toggle');
 const icons =  switchToogle.querySelectorAll('.material-symbols-outline');
-
+const input = document.getElementById("input-busca")
 switchToogle.addEventListener('click', () => {
     
     document.body.classList.toggle('dark-theme-variables');
@@ -11,8 +11,59 @@ switchToogle.addEventListener('click', () => {
     })
 
 
+
+
+
+
+
 })
     
+function botaoDeBusca (){
+    const inputValue = input.value;
+    
+    movimentoInput(inputValue);
+}
+
+
+function fecharInput(){
+    input.style.visibility = 'hidden';
+    input.style.width = '40px';
+    input.style.padding = '0.5rem 0.5rem 0.5rem 2.6rem';
+    input.style.transition = 'all 0.5s ease-in-out 0s'
+    input.value = "";
+  
+}
+
+function abrirInput(){
+    input.style.visibility = 'visible';
+    input.style.width = '159px';
+    input.style.padding = '0.5rem 0.5rem 0.5rem 3.1rem';
+    input.style.transition = 'all 0.5s ease-in-out 0s'
+    input.value = "";
+   
+}
+
+function movimentoInput(inputValue){
+   const visibility = input.style.visibility;
+    /* visibility == 'hidden' ? abrirInput() : fecharInput(); */
+    //console.log(inputValue)
+
+   inputValue && procurarCidade(inputValue) ;
+    
+    if (visibility == "hidden"){
+        abrirInput()
+    }
+    else {
+        fecharInput()
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fecharInput();
+})
+
+
 let slides = document.querySelectorAll('.pdiv');
 let index = 0;
 
